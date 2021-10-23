b26975311ade0ae549e04c6ff76391d16d19dd19
magicznyprzemek <56158818+magicznyprzemek@users.noreply.github.com>
2021-10-23 17:10:01 +0200

    Add files via upload

---
+++ sem2_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace sem2_1
+{
+ 
+    class Program
+    {
+       public static void Main()
+       {
+            /*
+            Sprzedaz[] tab = { new Sprzedaz(12, 0.2),
+                               new Sprzedaz(65, 0.7),
+                               new Sprzedaz(65, 0.3),};
+            foreach(Sprzedaz sp in tab)
+            {
+               // Console.WriteLine(sp.ToString());     //z1/2
+            }
+           // Console.WriteLine(Sprzedaz.Min(tab)); // z3
+            */
+            //----------------------------
+
+            //zd1
+            
+            Prostokat[] tab = {
+                new Prostokat(12, 3),
+                new Prostokat(100, 37),
+                new Prostokat(10, 6)
+                                };
+            foreach(Prostokat pr in tab)
+            {
+                Console.WriteLine(pr.ToString());
+                
+            }
+            Console.WriteLine(Prostokat.Max(tab));
+
+
+
+        }
+    }
+}
---
+++ sem2_1/Sprzedaz.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sem2_1
+{
+    class Prostokat
+    {
+        private double x;
+        private double y;
+        public Prostokat(double dl, double szr)
+        {
+            x = dl;
+            y = szr;
+
+        }
+        private double Obwod()
+        {
+            return (x+y)*2;
+        }
+        private double Powierzchnia()
+        {
+            return x*y;
+        }
+        public override string ToString()
+        {
+            string s1 = "obwód: " + Obwod() + " Powierzchnia: " + Powierzchnia();
+            return s1;
+        }
+        public static double Max(Prostokat[] tab)
+        {
+            double max = 0;
+            foreach (Prostokat pr in tab)
+            {
+                if (pr.Powierzchnia() > max)
+                {
+                    max = pr.Powierzchnia();
+                }
+            }
+            return max;
+        }
+    }
+
+    class Sprzedaz
+    {
+        private double cena;
+        private double upust;
+        private const int szt = 10;
+
+        public Sprzedaz(double price, double vent)
+        {
+            cena = price;
+            upust = vent;
+        }
+
+        private double WartPrzed()
+        {
+            return cena*szt;
+        }
+
+        private double WartPo()
+        {
+            return WartPrzed() - (WartPrzed()*(upust));
+        }
+        public override string ToString()
+        {
+            string before = $"Wartość przed: cena: {WartPrzed():C};   upust: {upust:P}; sztuki: {szt}";
+            string after = $"Wartość po: cena: {WartPo():C};   upust: {upust:P}; sztuki: {szt}";
+            return before + Environment.NewLine + after;
+            
+        }
+        public static double Min (Sprzedaz[] tab)
+        {
+            double min = Double.MaxValue;
+            foreach(Sprzedaz sp in tab)
+            {
+                if(sp.WartPrzed()<min)
+                {               
+                    min = sp.WartPrzed();
+                }
+            }
+            return min;
+        }
+    }
+
+}
