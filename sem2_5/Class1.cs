e3f85a7740da95c0bfb25c083a076374fe011b7a
magicznyprzemek <56158818+magicznyprzemek@users.noreply.github.com>
2021-10-31 10:56:32 +0100

    Add files via upload

---
+++ sem2_5/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sem2_5
+{
+   public struct test
+    {
+
+    }
+}
---
+++ sem2_5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace sem2_5
+{
+    public struct Ulamki
+    {
+        
+        public string x, y;
+        public Ulamki(string p1, string p2)
+        {
+            x = p1;
+            y = p2;
+
+        }
+        public double Dziel(string ulam)
+        {
+            string[] dziel = ulam.Split('/');
+            double r = Convert.ToDouble(dziel[0]) / Convert.ToDouble(dziel[1]);
+            return r;
+
+        }
+        public double sum()
+        {
+           return (Dziel(x) + Dziel(y));
+        }
+    }
+    class Program
+    {         
+        static void Main(string[] args)
+        {
+            
+            string linia = Console.ReadLine().ToString();
+            string[] ulamki = linia.Split('+', '=');
+            Console.Write("{0}+{1}= ", ulamki[0], ulamki[1]);
+            Ulamki a = new Ulamki(ulamki[0], ulamki[1]);
+            Console.Write(a.sum());
+
+            
+
+        }
+    }
+}
