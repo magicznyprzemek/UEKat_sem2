23dd422cb63134908f4f624c4db22dae568b0278
magicznyprzemek <56158818+magicznyprzemek@users.noreply.github.com>
2021-11-01 18:23:20 +0100

    Add files via upload

---
+++ sem2_6_z1/sem2_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sem2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostokat p1 = new Prostokat(5, 10);
+            Blat b1 = new Blat(2, 6, 600);
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(b1.ToString());
+        }
+    }
+}
---
+++ sem2_6_z1/sem2_6/Prostokat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sem2_6
+{
+    class Prostokat
+    {
+        public double x, y;
+        public Prostokat(double w1, double w2)
+        {
+            x = w1;
+            y = w2;
+        }
+        public  double Pole()
+        {
+            return (x * y);
+        }
+        public override string ToString()
+        {
+            return $"Wymiar x: {x} | Wymiar y: {y} | Pole: {Pole()}";
+        }
+    }
+}
