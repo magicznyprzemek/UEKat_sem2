a937739e4cbd665181e82dcd74fd508e57539af8
magicznyprzemek <56158818+magicznyprzemek@users.noreply.github.com>
2021-11-01 18:23:47 +0100

    Add files via upload

---
+++ sem2_6_z1/Blat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sem2_6
+{
+    class Blat : Prostokat
+    {
+        double z;
+        public Blat(double x, double y, double z1) : base(x, y)
+        {
+            z = z1;
+        }
+        public double cena()
+        {
+            return base.Pole() * z;
+        }
+        public override string ToString()
+        {
+            return base.ToString()+ $" | Cena: {cena()}";
+        }
+
+    }
+}
