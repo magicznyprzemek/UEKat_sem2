592528c2ee4face9dea044d4c8f7e1c1347d1868
magicznyprzemek <56158818+magicznyprzemek@users.noreply.github.com>
2021-11-04 10:14:17 +0100

    Add files via upload

---
+++ sem2_78/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace sem2_78
+{
+    class Tablica <T>
+    {
+        public int indeks;
+        public T wartosc;
+        protected T[] tab = new T[2];
+        
+        public void wyswietl()
+        {
+            int xd = 0;
+            foreach(T x in tab)
+            {
+
+                Console.WriteLine(xd+" "+x);
+                xd++;
+            }
+        }
+        public void wpisz(int x, T p)
+        {
+            indeks = x;
+            wartosc = p;
+            if (indeks > tab.Length - 1)
+            {
+                Array.Resize(ref tab, indeks + 1);
+                tab[indeks] = wartosc;
+            }
+            else
+            {
+                tab[indeks] = wartosc;
+            }
+        }
+    }
+}
---
+++ sem2_78/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sem2_78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //z1
+            Tablica<string> tab1 = new Tablica<string>();
+            tab1.wpisz(0, "test");
+            tab1.wpisz(1, "test2");
+            tab1.wpisz(5, "test3");
+            tab1.wyswietl();
+
+        }
+
+    }
+}
