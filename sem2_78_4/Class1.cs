a73bb6fc1c1b2a157c3f5d1d944ac41916914745
magicznyprzemek <56158818+magicznyprzemek@users.noreply.github.com>
2021-11-06 15:22:01 +0100

    Add files via upload

---
+++ sem2_78_4/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sem2_78_4
+{
+    class auto
+    {
+        public int rok;
+        public string nazwa;
+        public auto(int x, string n)
+        {
+            rok = x;
+            nazwa = n;
+        }
+        public static bool compcar(auto t1, auto t2)
+        {
+            if(t1.rok>=t2.rok)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{rok}  {nazwa}";
+        }
+
+
+    }
+}
---
+++ sem2_78_4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace sem2_78_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            auto[] tab=
+            {
+                new auto(1999, "skoda"),
+                new auto(2000, "toyota"),
+                new auto(2012, "ford"),
+                new auto(2013, "ford 2")
+
+            };
+            compare<auto> comp = auto.compcar;
+            sort(tab, comp);
+            Array.ForEach(tab, x => { Console.WriteLine(x.ToString()); });
+                
+        }
+
+        public delegate bool compare<T>(T x, T y);
+
+        static void sort<T>(T[] arr, compare<T> comp) 
+        {
+            T temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (comp( arr[i], (arr[i + 1])))
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
