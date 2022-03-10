49164eacfb21fe72d7e1d5f64556715c5965af2d
magicznyprzemek <56158818+magicznyprzemek@users.noreply.github.com>
2022-03-10 13:52:41 +0100

    Add files via upload

---
+++ sem2_10_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sem2_10_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kolory k = new kolory("bialy", "czerwony", "zółty", "zielony", "czarny", "brązowy");
+            foreach (string kolor in k)
+            {
+                Console.WriteLine(kolor);
+            }
+        }
+
+    }
+}
---
+++ sem2_10_4/kolory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Linq;
+
+namespace sem2_10_4
+{
+    class kolory : IEnumerable
+    {
+        string[] teksty = null;
+        public kolory(params string[] tabParam)
+        {
+            teksty = new string[tabParam.Length];
+            int indeks = 0;
+            foreach (string txt in tabParam)
+            {
+                teksty[indeks++] = txt;
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return teksty.GetEnumerator();
+        }
+
+    }
+}
